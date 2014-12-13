75aaa46ca2ca2958a917bd67dbe8e4bd7f9f7d7a
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-13 21:09:52 +0700

    Show sample table for each border style

--- lib/Pod/Weaver/Section/ANSITable/BorderStyles.pm
+++ lib/Pod/Weaver/Section/ANSITable/BorderStyles.pm
@@ -36,13 +36,36 @@ sub weave_section {
     {
         no strict 'refs';
         my $border_styles = \%{"$pkg\::border_styles"};
-        $text = "=over\n\n";
-        for my $style (sort keys %h) {
+        $text = "";
+        for my $style (sort keys %$border_styles) {
             my $spec = $border_styles->{$style};
-            $text .= "=item * $short_pkg\::$style ($spec->{summary})\n\n";
+            $text .= "=head2 $short_pkg\::$style\n\n";
+            $text .= "$spec->{summary} " if $spec->{summary};
+            $text .= join(
+                "",
+                "(utf8: ", ($spec->{utf8} ? "yes":"no"), ", ",
+                "box_chars: ", ($spec->{box_chars} ? "yes":"no"),
+                ").\n\n",
+            );
             $text .= "$spec->{description}\n\n" if $spec->{description};
+            next if $spec->{box_chars};
+            # show sample table
+            local $ENV{COLUMNS} = 80;
+            my $t = Text::ANSITable->new(
+                use_color => 0,
+                use_utf8  => 1,
+                use_box_chars => 0,
+                columns   => [qw/column1 column2/],
+            );
+            $t->border_style("$short_pkg\::$style");
+            $t->add_row(['row1.1', 'row1.2']);
+            $t->add_row(['row2.1', 'row3.2']);
+            $t->add_row_separator;
+            $t->add_row(['row3.1', 'row3.2']);
+            my $t_text = $t->draw;
+            $t_text =~ s/^/ /gm;
+            $text .= $t_text . "\n\n";
         }
-        $text .= "=back\n\n";
     }
 
     $document->children->push(
@@ -50,10 +73,11 @@ sub weave_section {
             command  => 'head1',
             content  => 'INCLUDED BORDER STYLES',
             children => [
-                map { s/\n/ /g; Pod::Elemental::Element::Pod5::Ordinary->new({ content => $_ })} split /\n\n/, $text
+                map { Pod::Elemental::Element::Pod5::Ordinary->new({ content => $_ })} split /\n\n/, $text
             ],
         }),
     );
+    $self->log(["Inserted INCLUDED BORDER STYLES POD section to file %s", $filename]);
 }
 
 no Moose;
